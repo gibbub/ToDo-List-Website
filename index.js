db420e6fbe1682cb6e27611c211a7428ad9cdcec
Gabby Albrecht <galbrecht28@gmail.com>
2023-07-07 12:59:35 -0400

    Maintain order of crossed-out items when updated

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ $("#list-title").html(today.toLocaleDateString("en-US", options) + "<br/> To Do 
 
 const items = [];
 const checkedBoxes = [];
+let numChecked = 0;
 
 // Adding
 
@@ -53,32 +54,40 @@ $(document).on("click", ".delete-button", function () {
 
 function deleteItem(item) {
     items.splice(item.value, 1);
+    if (checkedBoxes[item.value] === "checked") {
+        numChecked--;
+    }
+    else {
+        motivate();
+    }
     checkedBoxes.splice(item.value, 1);
     updateList();
-    motivate();
 }
-
-// TODO: Maintain crossed out text when updating list
 
 // Checking
 
-$(document).on("click", ".todo-item :checked", function() {
+$(document).on("click", ".todo-item input", function() {
     if (this.checked) {
+        checkedBoxes[this.value] = "checked";
+        numChecked++;
         motivate();
+    }
+    else {
+        checkedBoxes[this.value] = "";
     }
 });
 
 // Updating
 
 function updateList() {
-    console.log(checkedBoxes);
     // clear list
     $(".todo-item ").remove();
 
     // update list
     for (let i = 0; i < items.length; i++) {
-        $("#todo-list").append("<div class='item todo-item'><input type='checkbox'><p>"+ items[i] +"</p><form method='post'><button class='delete-button' type='button' value="+ i.toString() +">✖</button></form></div>");
-    }
+        let checked = checkedBoxes[i];
+        $("#todo-list").append(`<div class='item todo-item'><input type='checkbox' value=${i.toString()} ${checked}><p>${items[i]}</p><form method='post'><button class='delete-button' type='button' value=${i.toString()}>✖</button></form></div>`);
+    }    
 }
 
 
@@ -88,7 +97,7 @@ function motivate() {
     let tasksLeft = items.length;
     let messages = [];
     
-    if (tasksLeft === 0) {
+    if (tasksLeft === 0 || (numChecked === checkedBoxes.length && checkedBoxes.length > 0)) {
         messages = [
             "Yayyy! All done!",
             "You did it! Now go take a break.",
@@ -107,7 +116,7 @@ function motivate() {
         ];
     }
 
-    if (tasksLeft > 0 && tasksLeft < 4) {
+    if ((tasksLeft > 0 && tasksLeft < 4) && numChecked === 0) {
         messages.push("Only "+ tasksLeft +" left!");
         messages.push("You're almost there!");
         messages.push(tasksLeft +" left to go.");
